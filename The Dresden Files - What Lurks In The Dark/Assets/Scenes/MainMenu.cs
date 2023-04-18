b81465510a73e8441b24efc89b129b97cf1de295
kms2096 <kms2096@g.rit.edu>
2023-04-17 22:43:49 -0400

    Main Menus finished
    Still working on Pause Menu

--- The Dresden Files - What Lurks In The Dark/Assets/Scenes/MainMenu.cs
+++ The Dresden Files - What Lurks In The Dark/Assets/Scenes/MainMenu.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void StartGame()
     {
-        
+        SceneManager.LoadScene("DreamOne");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void QuitGame()
     {
-        
+        Application.Quit();
     }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void GoToCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    //public void Resume()
+    //{
+
+    //}
+
+    //public void Pause()
+    //{
+
+    //}
 }
